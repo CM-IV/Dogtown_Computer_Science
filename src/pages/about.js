9dc62487e80c5674040dd11569a9ee7121d0f57e
Charlie Mathis IV <cmiv@pop-os.localdomain>
2020-02-21 16:01:10 -0600

    About dividing line moved

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,8 +10,8 @@ const About = () => (
         <h1 class="title is-1">Welcome to the about page.</h1>
         <h3 class="title is-3">This website was created by Charlie with Dogtown Computer Science.</h3>
         <h4 class="title is-4">Business inquiries/questions? Contact me here.</h4>
-        <hr/>
         <a class="button" href="mailto:cmathIV@protonmail.com">Email me</a>
+        <hr/>
         <p>Created on Friday December 20, 2019</p>
     </Layout>
 )
